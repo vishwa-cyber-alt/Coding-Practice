a1f7b94811c055014ef207b51a3b898d4a1f8594
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2024-02-18 11:58:41 +0530

    Create method.java

---
+++ method.java
@@ -0,0 +1,13 @@
+
+class Sum {
+    void add()
+    {
+        int a=34,b=6,c;
+        c=a+b;
+        System.out.print(c);
+    }
+    public static void main(String[] args) {
+       Sum s=new Sum();
+       s.add();
+    }
+}
