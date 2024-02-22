e5321c0559ff0b6a4dcac97ba3ff290d1bcf9ea2
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2024-02-22 18:36:42 +0530

    Create sum of n.py

---
+++ sum of n.py
@@ -0,0 +1,14 @@
+"""
+Sum of 'N' natural numbers
+Max Score: 7 | Difficulty: Not Specified
+Take a number (n) as input and print sum of the n natural numbers
+
+Input:
+10
+Output:
+55
+"""
+s=0
+for i in range(1,11):
+  s+=i
+print(s)
